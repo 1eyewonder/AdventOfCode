1c167546a810163bb9d24c121a69a6611e8023b6
1eyewonder <bronsonbata@gmail.com>
2023-12-26 15:44:38 -0600

    Updated to use fsx instead of ipynb

---
+++ 2023/Day1/part1.fsx
@@ -0,0 +1,24 @@
+open System
+open System.IO
+
+let puzzleInput = File.ReadAllText("data.txt")
+let getLines (text: string) = text.Split("\n")
+
+let rec getFirstDigitFromLine (text: char list) =
+    match text with
+    | [] -> None
+    | head :: tail ->
+        match Int32.TryParse(string head) with
+        | true, _ -> Some(string head)
+        | _ -> getFirstDigitFromLine tail
+
+getLines puzzleInput
+|> Array.choose (fun line ->
+    let firstDigit = List.ofSeq line |> getFirstDigitFromLine
+    let secondDigit = List.ofSeq line |> List.rev |> getFirstDigitFromLine
+
+    match firstDigit, secondDigit with
+    | Some firstDigit, Some secondDigit -> sprintf "%s%s" firstDigit secondDigit |> int |> Some
+    | _ -> None)
+|> Array.fold (+) 0
+|> printfn "%i"
